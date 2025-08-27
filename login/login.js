28ab8c0e51cd251a6ebbef4fa1ee5bb1e6b312ba
Ricardo Washington Rodrigues de souza <r.washington.r.s@gmail.com>
2025-08-27 20:11:13 -0300

    fire base

--- login/login.js
+++ login/login.js
@@ -15,3 +15,24 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         messageDiv.textContent = '';
     }
 });
+
+function login() {
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const messageDiv = document.getElementById('registerMessage');
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Login bem-sucedido
+            console.log('Login bem-sucedido:', userCredential);
+            window.location.href = "../home/home.html";
+        })
+        .catch((error) => {
+            // Tratar erros de login
+            console.error('Erro ao fazer login:', error);
+            messageDiv.textContent = 'Erro ao fazer login: ' + error.message;
+            messageDiv.style.color = 'red';
+        });
+}
+
+console.log(firebase.apps.length ? 'Firebase inicializado' : 'Firebase não inicializado');
