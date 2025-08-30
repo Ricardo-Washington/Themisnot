6b2ca380b61179d65b642fad03cfffad794fc2d7
Ricardo Washington Rodrigues de souza <r.washington.r.s@gmail.com>
2025-08-30 10:19:02 -0300

    arrumou

--- Registrice/registre-se.js
+++ Registrice/registre-se.js
@@ -1,30 +1,31 @@
-// Configuração do Firebase
-const firebaseConfig = {
+  // Configuração do Firebase
+  const firebaseConfig = {
   apiKey: "AIzaSyDVK6hzW1gifPrjDQrC00I9GWQqumaY3PE",
   authDomain: "themis-bcaa4.firebaseapp.com",
   projectId: "themis-bcaa4",
   storageBucket: "themis-bcaa4.firebasestorage.app",
   messagingSenderId: "244101876997",
   appId: "1:244101876997:web:fcee5a85d3506417c7fb6c"
-};
-firebase.initializeApp(firebaseConfig);
-// Inicialize o Firebase
-if (!firebase.apps.length) {
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  // Inicialize o Firebase
+  if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-} else {
+  } else {
     firebase.app(); // Use a instância já inicializada
-}
+  }
 
-// Inicialize o Firestore
-const db = firebase.firestore();
+  // Inicialize o Firestore
+  const db = firebase.firestore();
 
-// Adiciona o listener para o formulário
-document.getElementById('registerForm').addEventListener('submit', function(event) {
+  // Adiciona o listener para o formulário
+  document.getElementById('registerForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Impede o envio padrão do formulário
   registerUser(); // Chama a função de registro
-});
+  });
 
-function registerUser() {
+  function registerUser() {
   const email = document.getElementById('email').value.trim();
   const confirmEmail = document.getElementById('confirmEmail').value.trim();
   const password = document.getElementById('password').value;
@@ -43,21 +44,23 @@ function registerUser() {
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-
       // Salva os dados do usuário no Firestore
-      return db.collection('users').doc(user.uid).set({
+      const logado =  db.collection('users').doc(user.uid).set({
         email: user.email,
         createdAt: new Date()
       });
-      window.location.href = '/home/home.html';
+    })
+    .then(() => {
+      alert('Registro realizado com sucesso!');
+      window.location.href = '/login/login.html'; // Redireciona para a página de login
     })
     .catch(error => {
       console.error('Erro ao registrar:', error);
       alert(getErrorMessage(error));
     });
-}
-
-function getErrorMessage(error) {
+  }
+  // Função para mapear códigos de erro para mensagens amigáveis
+  function getErrorMessage(error) {
   if (error.code === 'auth/email-already-in-use') {
     return 'Este e-mail já está em uso.';
   } else if (error.code === 'auth/invalid-email') {
@@ -67,4 +70,4 @@ function getErrorMessage(error) {
   } else {
     return error.message;
   }
-}
+  }
